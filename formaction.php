5eb11f7a78d51d299169c12630abeffef6b24c85
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2017-06-14 17:16:41 +0530

    authentication done

---
+++ formaction.php
@@ -0,0 +1,13 @@
+<?php
+$message="";
+if(count($_POST)>0) {
+	$conn = mysqli_connect("localhost","root","","network");
+	$result = mysqli_query($conn,"SELECT * FROM login WHERE user_name='" . $_POST["userName"] . "' and password = '". $_POST["password"]."'");
+	$count  = mysqli_num_rows($result);
+	if($count==0) {
+		$message = "Invalid Username or Password!";
+	} else {
+		$message = "You are successfully logged in!";
+	}
+}
+?>
---
+++ signup.php
@@ -0,0 +1,32 @@
+
+<html>
+<head>
+<link href="second.css" rel="stylesheet" type="text/css" >
+<title>
+signup
+</title>
+</head>
+<body>
+<h1>CREATE A NEW ACCOUNT</h1>
+<div class="form">
+<form action="welcome.php" method="post" >
+	
+
+USERNAME:<input type="text" name="username" placeholder="USERNAME" required><br><br>
+PASSWORD:<input type="password" name="password" placeholder="PASSWORD" required><br><br>
+
+
+
+<input type='submit' >
+
+
+
+</form>
+
+</div>
+
+
+</body>
+
+</html>
+
